12d11ca7d0e7e1c93d359b240cc9f46a84591d7b
Modestas <modestask97@gmail.com>
2020-01-31 16:29:05 +0000

    login functionality

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const jwtSecret = require('config').get('jwtSecret')
+const { check, validationResult } = require('express-validator')
 
 const auth = require('../../middleware/auth')
 const User = require('../../models/User')
@@ -14,6 +18,58 @@ router.get('/', auth, async (req, res) => {
 		const user = await User.findById(req.user.id).select('-password')
 		// return user data
 		res.json(user)
+	} catch (err) {
+		console.error(err.message)
+		res.status(500).send('Server Error')
+	}
+})
+
+//  middleware to handle errors / validation
+const validation = [
+	check('email', 'Please Include a valid email').isEmail(),
+	check('password', 'Password is required').exists()
+]
+
+// @route POST api/auth
+// @desc  login and get token
+// @acess Public
+router.post('/', validation, async (req, res) => {
+	const errors = validationResult(req)
+	// if there are errors, send back 400 with array of errors
+	if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() })
+
+	const { email, password } = req.body
+
+	try {
+		let user = await User.findOne({ email })
+
+		const Err400 = () => res.status(400).json({ errors: [{ msg: 'Invalid credentials' }] })
+
+		if (!user) return Err400()
+
+		// compare password user has typed in to the one on the Db
+		const passwordMatches = await bcrypt.compare(password, user.password)
+
+		if (!passwordMatches) return Err400()
+
+		// mongoose provides id alias to mongodb _id
+		const payload = {
+			user: {
+				id: user.id
+			}
+		}
+		jwt.sign(
+			payload,
+			jwtSecret,
+			// {
+			// 	// 1 hour
+			// 	expiresIn: 3600
+			// },
+			(err, token) => {
+				if (err) throw err
+				res.json({ token })
+			}
+		)
 	} catch (err) {
 		console.error(err.message)
 		res.status(500).send('Server Error')
--- routes/api/users.js
+++ routes/api/users.js
@@ -3,17 +3,13 @@ const router = express.Router()
 const gravatar = require('gravatar')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const jwtSecret = require('config').get('jwtSecret')
 const { check, validationResult } = require('express-validator')
 
 const User = require('../../models/User')
-const jwtSecret = require('config').get('jwtSecret')
-
-// @route POST api/users
-// @desc register user
-// @acess Public
 
 //  middleware to handle errors / validation
-const validateUser = [
+const validation = [
 	check('name', 'Name is required')
 		.not()
 		.isEmpty(),
@@ -21,7 +17,10 @@ const validateUser = [
 	check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
 ]
 
-router.post('/', validateUser, async (req, res) => {
+// @route POST api/users
+// @desc register user
+// @acess Public
+router.post('/', validation, async (req, res) => {
 	const errors = validationResult(req)
 	// if there are errors, send back 400 with array of errors
 	if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() })
@@ -30,10 +29,8 @@ router.post('/', validateUser, async (req, res) => {
 
 	try {
 		let user = await User.findOne({ email })
-
 		// if user already exists send back error message mimicking same format as express-validator
 		if (user) return res.status(400).json({ errors: [{ msg: 'User already exists' }] })
-
 		// get users gravatar
 		const avatar = gravatar.url(email, {
 			// size
@@ -43,7 +40,6 @@ router.post('/', validateUser, async (req, res) => {
 			// default image
 			d: 'mm'
 		})
-
 		// create new instance of user
 		user = new User({
 			name,
@@ -51,22 +47,18 @@ router.post('/', validateUser, async (req, res) => {
 			password,
 			avatar
 		})
-
 		// encrypt password
 		// salt adds random characters to a hashed password (10 in this case)
 		const salt = await bcrypt.genSalt(10)
 		user.password = await bcrypt.hash(password, salt)
-
 		// save user to db
 		await user.save()
-
 		// mongoose provides id alias to mongodb _id
 		const payload = {
 			user: {
 				id: user.id
 			}
 		}
-
 		jwt.sign(
 			payload,
 			jwtSecret,
@@ -79,7 +71,6 @@ router.post('/', validateUser, async (req, res) => {
 				res.json({ token })
 			}
 		)
-
 		// return jwt
 		res.send('User registered')
 	} catch (err) {
