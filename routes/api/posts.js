07bfe696aa95bb2fdcadaa2b509bc52e709e5091
Modestas <modestask97@gmail.com>
2020-02-01 18:21:13 +0000

    post comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,10 +4,10 @@ const { check, validationResult } = require('express-validator')
 const auth = require('../../middleware/auth')
 
 const Post = require('../../models/Post')
-const Profile = require('../../models/Profile')
 const User = require('../../models/User')
 
-const createPostValidaton = [
+// used for creating posts and comments
+const validation = [
 	check('text', 'Text is required')
 		.not()
 		.isEmpty()
@@ -16,7 +16,7 @@ const createPostValidaton = [
 // @route POST api/posts
 // @desc Create a post
 // @acess Private
-router.post('/', [auth, createPostValidaton], async (req, res) => {
+router.post('/', [auth, validation], async (req, res) => {
 	const errors = validationResult(req)
 	if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() })
 	try {
@@ -133,5 +133,61 @@ router.put('/unlike/:id', auth, async (req, res) => {
 		res.status(500).send('Server Error')
 	}
 })
+
+// @route POST api/posts/comment/:id
+// @desc comment on post
+// @acess Private
+router.put('/unlike/:id', auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id)
+		// check if post has not yet been liked
+		if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+			return res.status(500).json({ msg: 'Post has not yet been liked' })
+		}
+		// remove like from likes array
+		const index = post.likes.map(like => like.user.toString()).indexOf(req.user.id)
+		post.likes.splice(index, 1)
+
+		await post.save()
+		res.json(post.likes)
+		//
+	} catch (err) {
+		console.log(err.message)
+		res.status(500).send('Server Error')
+	}
+})
+
+// @route POST api/posts/comment/:id
+// @desc comment on a post
+// @access Private
+router.post('/comment/:id', [auth, validation], async (req, res) => {
+	const errors = validationResult(req)
+	if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() })
+	try {
+		// get all properites of user from db except password
+		const user = await User.findById(req.user.id).select('-password')
+		const post = await Post.findById(req.params.id)
+
+		// construct the comment with data you need
+		const newComment = {
+			// text is the only user generated property in this case
+			text: req.body.text,
+			name: user.name,
+			avatar: user.avatar,
+			user: req.user.id
+		}
+		// add comment to start of comments array
+		post.comments.unshift(newComment)
+		await post.save()
+		res.json(post)
+	} catch (err) {
+		console.log(err.message)
+		res.status(500).send('Server Error')
+	}
+})
+
+// @route DELETE api/posts/comment/:id
+// @desc comment on a post
+// @access Private
 
 module.exports = router
