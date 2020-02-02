755361cc758be69050e982d23ddad5592c95162b
Modestas <modestask97@gmail.com>
2020-02-02 11:58:20 +0000

    auth routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
 import './App.css'
 
 const App = () => (
-	<>
+	<Router>
 		<Navbar />
-		<Landing />
-	</>
+		<Route exact path='/' component={Landing} />
+		{/* every page will have a container except landing page for image to fill screen */}
+		<section className='container'>
+			<Switch>
+				<Route exact path='/login' component={Login} />
+				<Route exact path='/register' component={Register} />
+			</Switch>
+		</section>
+	</Router>
 )
 
 export default App
