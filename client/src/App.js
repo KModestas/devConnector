d3059bc3fa2fc5fa8e0bf4b8302bfc045cd1cc07
Modestas <modestask97@gmail.com>
2020-02-05 12:50:47 +0000

    connect post reducer

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import AddExperience from './components/profile/forms/AddExperience'
 import AddEducation from './components/profile/forms/AddEducation'
 import Profiles from './components/profiles/Profiles'
 import Profile from './components/profile/Profile'
+import Posts from './components/posts/Posts'
 
 import setAuthToken from './utils/setAuthToken'
 import { loadUser } from './actions/auth'
@@ -47,6 +48,7 @@ const App = () => {
 						<PrivateRoute exact path='/edit-profile' component={EditProfile} />
 						<PrivateRoute exact path='/add-experience' component={AddExperience} />
 						<PrivateRoute exact path='/add-education' component={AddEducation} />
+						<PrivateRoute exact path='/posts' component={Posts} />
 					</Switch>
 				</section>
 			</Router>
