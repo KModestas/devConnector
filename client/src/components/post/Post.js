a2a55730041e8eaf3daae735215dfaf7fecb8b31
Modestas <modestask97@gmail.com>
2020-02-05 14:23:57 +0000

    comment form

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
 import PostItem from '../posts/PostItem'
-// import CommentForm from '../post/CommentForm'
+import CommentForm from '../post/CommentForm'
 // import CommentItem from '../post/CommentItem'
 import { getPost } from '../../actions/post'
 
@@ -22,12 +22,12 @@ const Post = ({ getPost, post: { post, loading }, match: { params } }) => {
 			</Link>
 			{/* if show actions is false, like and delete functionality will not show so we can display a single post without those actions */}
 			<PostItem post={post} showActions={false} />
-			{/* <CommentForm postId={post._id} />
-			<div className='comments'>
+			<CommentForm postId={post._id} />
+			{/* <div className='comments'>
 				{post.comments.map(comment => (
 					<CommentItem key={comment._id} comment={comment} postId={post._id} />
 				))}
-			</div> */}
+			</div>  */}
 		</>
 	)
 }
