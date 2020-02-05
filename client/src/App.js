5250d4f3f9215a1710713c0bc1091fdcf6c98072
Modestas <modestask97@gmail.com>
2020-02-05 10:54:48 +0000

    profiles display

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,18 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store'
 
+import PrivateRoute from './components/auth/PrivateRoute'
 import Alert from './components/layout/Alert'
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
-import PrivateRoute from './components/auth/PrivateRoute'
 import Dashboard from './components/dashboard/Dashboard'
 import CreateProfile from './components/profile/forms/CreateProfile'
 import EditProfile from './components/profile/forms/EditProfile'
 import AddExperience from './components/profile/forms/AddExperience'
 import AddEducation from './components/profile/forms/AddEducation'
+import Profiles from './components/profiles/Profiles'
 
 import setAuthToken from './utils/setAuthToken'
 import { loadUser } from './actions/auth'
@@ -37,9 +38,8 @@ const App = () => {
 					<Alert />
 					<Switch>
 						<Route exact path='/login' component={Login} />
-						<Route exact path='/login' component={Login} />
-						<Route exact path='/login' component={Login} />
 						<Route exact path='/register' component={Register} />
+						<Route exact path='/profiles' component={Profiles} />
 						<PrivateRoute exact path='/dashboard' component={Dashboard} />
 						<PrivateRoute exact path='/create-profile' component={CreateProfile} />
 						<PrivateRoute exact path='/edit-profile' component={EditProfile} />
